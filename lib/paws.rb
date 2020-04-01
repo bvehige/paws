22d44ec3163d8fd78a1599ed146b0d632441e0a3
Brian Vehige <brianvehige@gmail.com>
2020-04-01 02:31:30 +0000

    added nokogiri and pry, plus gemspec edits

--- lib/paws.rb
+++ lib/paws.rb
@@ -1,5 +1,10 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative "./paws/version"
 require_relative "./paws/cli"
 require_relative "./paws/dog"
+require_relative "./paws/paws_scraper"
 
 
---
+++ lib/paws/paws_scraper.rb
@@ -0,0 +1,13 @@
+class Paws::Paws_scraper
+
+def dog_scraper
+  dogs = []
+  
+  
+
+  
+  dogs
+end
+
+
+end
