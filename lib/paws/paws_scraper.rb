3e8abca771a392d16c5b3615c401769bc2b9ba7a
Brian Vehige <brianvehige@gmail.com>
2020-04-01 02:53:46 +0000

    saving progress

--- lib/paws/paws_scraper.rb
+++ lib/paws/paws_scraper.rb
@@ -1,11 +1,12 @@
+require 'pry'
+require 'Nokogiri'
+
 class Paws::Paws_scraper
 
 def dog_scraper
   dogs = []
-  
-  
+  doc = Nokogiri::HTML(open("https://www.paws.org/adopt/dogs/")
 
-  
   dogs
 end
 
