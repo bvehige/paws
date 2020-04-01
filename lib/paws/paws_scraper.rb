5932ea700287812df4cb7cbffa6505b301e3e63a
Brian Vehige <brianvehige@gmail.com>
2020-04-01 06:04:14 +0000

    correting syntax issues

--- lib/paws/paws_scraper.rb
+++ lib/paws/paws_scraper.rb
@@ -1,11 +1,11 @@
-require 'pry'
-require 'Nokogiri'
+#require 'pry'
+#require 'Nokogiri'
 
 class Paws::Paws_scraper
 
 def dog_scraper
   dogs = []
-  doc = Nokogiri::HTML(open("https://www.paws.org/adopt/dogs/")
+  doc = Nokogiri::HTML(open("https://www.paws.org/adopt/dogs/"))
 
   dogs
 end
